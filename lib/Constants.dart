ae05b866ce5834601c10a811a72eac8c642d61d5
faizan-19 <76480695+faizan-19@users.noreply.github.com>
2021-05-26 16:30:00 +0500

    login 2

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Constants extends GetxController {
-  final BlueColor = Color(0x00203FFF);
-}
+final fontClr = Color(0xFF18446D);
+final frontClr = Color(0xFFEEEEEE);
+final backgroundClr = Color(0xFF18446D);
+final fontStl = GoogleFonts.chelaOne();
+//final t = Color(Colors.)
--- lib/Login.dart
+++ lib/Login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:medicoze/Constants.dart';
 
 class Login extends StatefulWidget {
@@ -9,24 +9,23 @@ class Login extends StatefulWidget {
 }
 
 class _Login extends State<Login> {
-  Constants constants = Get.put(Constants());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: constants.BlueColor,
+      backgroundColor: backgroundClr,
       body: Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Center(
               child: Container(
-                height: 500,
-                width: 300,
+                height: 600,
+                width: 350,
                 child: Card(
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(30)),
                   elevation: 15,
-                  color: Colors.white,
+                  color: frontClr,
                   child: Column(
                     children: [
                       /* ------------------------------- Login Feild ------------------------------ */
@@ -36,7 +35,9 @@ class _Login extends State<Login> {
                           child: Text(
                             "Login",
                             style: TextStyle(
-                                color: Colors.brown[200], fontSize: 30),
+                              color: fontClr,
+                              fontSize: 60,
+                            ),
                           ),
                         ),
                       ),
@@ -67,6 +68,7 @@ class _Login extends State<Login> {
                                   borderRadius: BorderRadius.circular(30))),
                         ),
                       ),
+                      /* ----------------------------- Forget Password ---------------------------- */
                       Padding(
                         padding: const EdgeInsets.symmetric(
                             vertical: 8, horizontal: 15),
@@ -76,13 +78,75 @@ class _Login extends State<Login> {
                             child: Container(
                               child: Text(
                                 "Forget Paswword?",
-                                style: TextStyle(color: Colors.brown[200]),
+                                style: TextStyle(color: fontClr),
                               ),
                             ),
                           ),
                         ),
                       ),
-                      InkWell(child: Card(child: Text("Login"))),
+                      /* ------------------------------ Login Button ------------------------------ */
+                      InkWell(
+                          child: Card(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50)),
+                              color: fontClr,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 130, vertical: 10),
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    color: frontClr,
+                                    fontSize: 26,
+                                  ),
+                                ),
+                              ))),
+                      Text(
+                        "or",
+                        style: TextStyle(color: fontClr, fontSize: 20),
+                      ),
+                      /* --------------------------- SignIn with Google/facebook --------------------------- */
+                      Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Container(
+                              margin: EdgeInsets.only(
+                                  bottom: 10, right: 10, left: 10),
+                              child: Row(children: [
+                                Text(
+                                  "Log In With:",
+                                  style: TextStyle(
+                                      color: fontClr,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                IconButton(
+                                    icon: Icon(
+                                      FontAwesome.facebook,
+                                      color: fontClr,
+                                    ),
+                                    onPressed: () {}),
+                                IconButton(
+                                    icon: Icon(
+                                      FontAwesome.google,
+                                      color: fontClr,
+                                    ),
+                                    onPressed: () {})
+                              ]))),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 60),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Don't have an account? ",
+                            ),
+                            InkWell(
+                                child: Text(
+                              "SignUp",
+                              style: TextStyle(color: fontClr),
+                            ))
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 ),
